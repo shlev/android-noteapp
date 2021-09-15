2b5fce251949bbcbae2ac35407d3d319b53728ac
shlomi <shl.lev.ks@gmail.com>
2021-09-15 13:10:25 +0300

    init repository insert initial data to db.

--- app/src/main/java/com/example/android_noteapp/NoteDao.java
+++ app/src/main/java/com/example/android_noteapp/NoteDao.java
@@ -1,10 +1,13 @@
 package com.example.android_noteapp;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface NoteDao {
@@ -20,5 +23,8 @@ public interface NoteDao {
 
     @Query("DELETE FROM note_table")
     void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
 
 }
